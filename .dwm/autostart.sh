3045875517a975f426734eb7c224a0b871e45394
marcocappelloni <marco.cappelloni@gmail.com>
2025-05-14 15:32:14 +0100

    added sxhkd to the autostart

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,22 +2,20 @@
 
 xset r rate 300 40 &
 
-#xsetroot -name "pippo" &
 exec slstatus &
 
 nitrogen --restore &
 
 # compositor
-#picom --animations -b &
 picom --config ~/.config/picom.conf &
 
 # sxhkd
 # (re)load sxhkd for keybinds
-#if hash sxhkd >/dev/null 2>&1; then
-#  pkill sxhkd
-#  sleep 0.5
-#  sxhkd -c "$HOME/.config/suckless/sxhkd/sxhkdrc" &
-#fi
+if hash sxhkd >/dev/null 2>&1; then
+  pkill sxhkd
+  sleep 0.5
+  sxhkd -c "$HOME/packages/suckless/sxhkd/sxhkdrc" &
+fi
 
 # Notifications
 dunst &
