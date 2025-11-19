4e0e4b0e4cd6b6db3bb2edc6c63aca3032733b41
marcocappelloni <marco.cappelloni@gmail.com>
2025-11-19 18:15:09 +0000

    added temperatures icon to the i3 status bar

---
+++ .config/i3/scripts/temperatures.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+input=$(temperatures_icon)
+color_code=$(echo "$input" | sed 's/\^c#\([[:xdigit:]]\{6\}\)\^.*/#\1/')
+icon=$(echo "$input" | sed 's/\^c#[[:xdigit:]]\{6\}\^\(.*\)/\1/')
+
+echo "<span foreground='$color_code'>$icon</span>"
