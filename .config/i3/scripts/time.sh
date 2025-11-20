b2dcc0ee01cdefba80f321f598d8074a9d9b03b8
marcocappelloni <marco.cappelloni@gmail.com>
2025-11-20 09:45:34 +0000

    Changed color of the timer in the status bar

--- .config/i3/scripts/time.sh
+++ .config/i3/scripts/time.sh
@@ -2,6 +2,6 @@
 # In your ~/.local/bin/click_block.sh script
 
 # Run the color script and capture its output
-CLICK_COLOR=$(get_xresource_color "color1")
+CLICK_COLOR=$(get_xresource_color "color7")
 
 echo "<span foreground='$CLICK_COLOR'>$(date '+%a %d %b %H:%M')</span>"
