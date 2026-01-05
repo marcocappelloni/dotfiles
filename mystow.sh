bc36dd99445a15df6e147bd91910a262c0b559ec
marcocappelloni <marco.cappelloni@gmail.com>
2026-01-05 22:00:00 +0000

    Added script to stow my packages

---
+++ mystow.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PACKAGES_TO_STOW=(
+  #"alacritty"
+  "bash"
+  "bat"
+  "dwm"
+  "fastfetch"
+  "ghostty"
+  #"helix"
+  "i3"
+  "kitty"
+  #"nvim"
+  "picom"
+  #"polybar"
+  #"qtile"
+  #"redshift"
+  "rofi"
+  "rofi_list"
+  "system"
+  "yazi"
+)
+
+for package in ${PACKAGES_TO_STOW[*]}; do
+  echo "   -> Stowing $package"
+  stow -v --adopt "$package"
+done
+
+echo "Do you want to do the restore of the git repository? (Y/N)"
+read answer
+if [[ ! "$answer" =~ ^[Yy]$ ]]; then
+  git restore .
+fi
