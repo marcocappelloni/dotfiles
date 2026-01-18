c0741830939e16af0fdd8259e82627418044a85c
marcocappelloni <marco.cappelloni@gmail.com>
2026-01-18 14:56:27 +0000

    Added bluetooth to polybar. Changed management of bspwm's rules. Added call for picom in bspwm and also the set to disable the screensaver

---
+++ bspwm/.config/bspwm/polybar/scripts/bluetooth/bluetooth.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
+  echo "%{F#66ffff}"
+else
+  if [ $(echo info | bluetoothctl | grep "Device " | wc -c) -eq 0 ]; then
+    echo ""
+  fi
+  echo "%{F#2193ff}"
+  #echo "%{F${xrdb:color4}}"
+fi
