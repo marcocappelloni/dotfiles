9a0c5a220bff3139fe84a8be55b5d14530def9b6
marcocappelloni <marco.cappelloni@gmail.com>
2026-01-20 22:51:40 +0000

    Fixed temperature icon for the polybar in bspwm

--- bspwm/.config/bspwm/polybar/scripts/temperatures.sh
+++ bspwm/.config/bspwm/polybar/scripts/temperatures.sh
@@ -3,5 +3,6 @@
 input=$(temperatures_icon)
 color_code=$(echo "$input" | sed 's/\^c#\([[:xdigit:]]\{6\}\)\^.*/#\1/')
 icon=$(echo "$input" | sed 's/\^c#[[:xdigit:]]\{6\}\^\(.*\)/\1/')
+trimmed_icon=$(echo "$icon" | tr -d '[:space:]')
 
-echo "%{F$color_code}$icon%{F-}"
+echo "%{F$color_code}$trimmed_icon%{F-}"
